3b2350055d136fffb2d98baa7d1868c349105059
isamukarasawa <isamukun3@gmail.com>
2022-02-23 12:49:50 +0900

    ユーザー機能と感想機能追加

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+
+Refile.secret_key = 'f61c91845f688102914f74c364b0a623cebf7b65c50e8fb1dbf9ac9e59889cb85867978b699180b0a157203c141e349793c0025ff307b0821c9b65fe6a3696f9'
